ccbba2a1e73a1065fe42817eb1c7b283c62e1dac
Simon Friedberger <simon@mozilla.com>
2023-03-15 10:23:45 +0100

    Add basic version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,203 @@
-fn main() {
-    println!("Hello, world!");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! This file implements a DAP client which will read task configurations from
+//! a JSON file and generate random measurements for infrastructure testing.
+
+use hpke::Deserializable;
+use hpke::{
+    aead::AesGcm128, kdf::HkdfSha256, kem::X25519HkdfSha256, Kem as KemTrait, OpModeS, Serializable,
+};
+use prio::vdaf::{prio3::Prio3Aes128CountVec, Client};
+use prio::{
+    codec::{Decode, Encode},
+    vdaf::prio3::Prio3Aes128Sum,
+};
+use rand::{thread_rng, Rng, SeedableRng};
+use reqwest::StatusCode;
+use serde::Deserialize;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, Write},
+};
+use types::{DAPHpkeInfo, HpkeCiphertext, Report, ReportMetadata};
+
+mod types;
+use crate::types::{DAPRole, HpkeConfig, ReportId, TaskId, Time, DAPAAD};
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    leader_url: String,
+    helper_url: String,
+    tasks: Vec<Task>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Task {
+    id: TaskId,
+    vdaf: String,
+    veclen: usize,
+    bits: u32,
+    report_count: usize,
+}
+
+fn read_config() -> Config {
+    let file = File::open("config.json").unwrap();
+    let json_data = serde_json::from_reader(file).expect("JSON was not well-formatted");
+    json_data
+}
+
+async fn get_hpke_config(base_url: &str, task_id: &TaskId) -> Result<HpkeConfig, Box<dyn Error>> {
+    let url = format!(
+        "{}/hpke_config?task_id={}",
+        base_url,
+        task_id.base64encoded()
+    );
+    println!("Getting HPKE config from: {}", url);
+    let resp = reqwest::get(url).await?;
+    let status = resp.status();
+    if status != StatusCode::OK {
+        panic!("Failed to get HPKE config: {:?}", &resp);
+    }
+    Ok(HpkeConfig::get_decoded(&resp.bytes().await?)?) // TODO use anyhow
+}
+
+fn dap_encrypt(
+    hpke_config: &HpkeConfig,
+    msg: &[u8],
+    aad: &DAPAAD,
+    info: &DAPHpkeInfo,
+) -> HpkeCiphertext {
+    let pubkey_bytes: &[u8] = &hpke_config.public_key;
+    let pubkey = <X25519HkdfSha256 as KemTrait>::PublicKey::from_bytes(pubkey_bytes).unwrap();
+    let mut csprng = rand::rngs::StdRng::from_entropy();
+    let (encapped_key, ciphertext) =
+        hpke::single_shot_seal::<AesGcm128, HkdfSha256, X25519HkdfSha256, _>(
+            &OpModeS::Base,
+            &pubkey,
+            info.bytes(),
+            msg,
+            aad.bytes(),
+            &mut csprng,
+        )
+        .unwrap();
+
+    HpkeCiphertext {
+        config_id: hpke_config.id,
+        enc: encapped_key.to_bytes().to_vec(),
+        payload: ciphertext,
+    }
+}
+
+async fn send_report(report: Report, config: &Config) -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let res = client
+        .post(format!("{}/upload", config.leader_url))
+        .header("Content-Type", "application/dap-report")
+        .body(report.get_encoded())
+        .send()
+        .await?;
+
+    if res.status() != StatusCode::OK {
+        println!("ERROR: Failed to send report. Response: {:?}", res);
+    }
+
+    Ok(())
+}
+
+async fn submit_reports_for_task(
+    task: &Task,
+    config: &Config,
+    leader_hpke_config: &HpkeConfig,
+    helper_hpke_config: &HpkeConfig,
+) {
+    // https://www.rfc-editor.org/rfc/rfc9180#name-kem-ids
+    assert_eq!(leader_hpke_config.kem_id, 0x0020);
+    assert_eq!(helper_hpke_config.kem_id, 0x0020);
+    // https://www.rfc-editor.org/rfc/rfc9180#name-kdf-ids
+    assert_eq!(leader_hpke_config.kdf_id, 1);
+    assert_eq!(helper_hpke_config.kdf_id, 1);
+    // https://www.rfc-editor.org/rfc/rfc9180#name-aead-ids
+    assert_eq!(leader_hpke_config.aead_id, 1);
+    assert_eq!(helper_hpke_config.aead_id, 1);
+
+    let (_, input_shares) = if task.vdaf == "Prio3Aes128CountVec" {
+        let prio = Prio3Aes128CountVec::new_aes128_count_vec(2, task.veclen).unwrap();
+        let mut measurement = vec![0; task.veclen];
+        measurement[0] = 1;
+        if thread_rng().gen::<bool>() {
+            measurement[2] = 1;
+        } else {
+            measurement[3] = 1;
+        }
+        prio.shard(&measurement).unwrap()
+    } else if task.vdaf == "Prio3Aes128Sum" {
+        let prio = Prio3Aes128Sum::new_aes128_sum(2, task.bits).unwrap();
+
+        let measurement = thread_rng().gen_range(0..1 << task.bits);
+        prio.shard(&measurement).unwrap()
+    } else {
+        panic!("Not implemented.");
+    };
+
+    debug_assert_eq!(input_shares.len(), 2);
+
+    let time_precision = 60;
+    let metadata = ReportMetadata {
+        report_id: ReportId::generate(),
+        time: Time::generate(time_precision),
+        extensions: vec![],
+    };
+
+    let public_share: Vec<u8> = Vec::new();
+    let aad = DAPAAD::new(&task.id, &metadata, &public_share);
+    let leader_payload = dap_encrypt(
+        &leader_hpke_config,
+        &input_shares[0].get_encoded(),
+        &aad,
+        &DAPHpkeInfo::new(DAPRole::Client, DAPRole::Leader),
+    );
+    let helper_info = DAPHpkeInfo::new(DAPRole::Client, DAPRole::Helper);
+    let helper_payload = dap_encrypt(
+        &helper_hpke_config,
+        &input_shares[1].get_encoded(),
+        &aad,
+        &helper_info,
+    );
+
+    let report = Report {
+        task_id: task.id.clone(),
+        metadata,
+        public_share,
+        encrypted_input_shares: vec![leader_payload, helper_payload],
+    };
+
+    send_report(report, &config).await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    let config = read_config();
+    for task in &config.tasks {
+        println!("Now processing: {:?}", task);
+
+        let (leader_hpke_config, helper_hpke_config) = tokio::join!(
+            get_hpke_config(&config.leader_url, &task.id),
+            get_hpke_config(&config.helper_url, &task.id)
+        );
+
+        let leader_hpke_config: HpkeConfig = leader_hpke_config.unwrap();
+        let helper_hpke_config: HpkeConfig = helper_hpke_config.unwrap();
+
+        print!("Submitting reports: ({}) ", task.report_count);
+        io::stdout().flush().unwrap();
+        for i in 0..task.report_count {
+            print!("{} ", i + 1);
+            io::stdout().flush().unwrap();
+            submit_reports_for_task(&task, &config, &leader_hpke_config, &helper_hpke_config).await;
+        }
+        println!("");
+    }
 }
---
+++ src/types.rs
@@ -0,0 +1,401 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! This file contains structs for use in the DAP protocol and implements TLS compatible
+//! serialization/deserialization as required for the wire protocol.
+//!
+//! The current draft standard with the definition of these structs is available here:
+//! https://github.com/ietf-wg-ppm/draft-ietf-ppm-dap
+
+use base64::engine::general_purpose;
+use base64::Engine;
+use prio::codec::{
+    decode_u16_items, decode_u32_items, encode_u16_items, encode_u32_items, CodecError, Decode,
+    Encode,
+};
+use serde::Deserialize;
+use std::io::{Cursor, Read};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use rand::Rng;
+
+pub enum DAPRole {
+    //Collector,
+    Client,
+    Leader,
+    Helper,
+}
+
+impl DAPRole {
+    pub fn as_byte(&self) -> u8 {
+        match *self {
+            //DAPRole::Collector => 0,
+            DAPRole::Client => 1,
+            DAPRole::Leader => 2,
+            DAPRole::Helper => 3,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DAPHpkeInfo {
+    data: Vec<u8>,
+}
+
+impl DAPHpkeInfo {
+    pub fn new(sender: DAPRole, receiver: DAPRole) -> Self {
+        let mut info: Vec<u8> = b"dap-02 input share".iter().copied().collect();
+        info.push(sender.as_byte());
+        info.push(receiver.as_byte());
+        DAPHpkeInfo { data: info }
+    }
+
+    pub fn bytes<'a>(&'a self) -> &'a [u8] {
+        &self.data
+    }
+}
+
+#[derive(Debug)]
+pub struct DAPAAD {
+    data: Vec<u8>,
+}
+
+impl DAPAAD {
+    pub fn new(task_id: &TaskId, metadata: &ReportMetadata, public_share: &Vec<u8>) -> Self {
+        let mut aad: Vec<u8> = Vec::new();
+        task_id.encode(&mut aad);
+        metadata.encode(&mut aad);
+        encode_u32_items(&mut aad, &(), public_share);
+        DAPAAD { data: aad }
+    }
+
+    pub fn bytes<'a>(&'a self) -> &'a [u8] {
+        &self.data
+    }
+}
+
+/// opaque TaskId[32];
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-task-configuration
+#[derive(Debug, PartialEq, Eq, Deserialize, Clone)]
+pub struct TaskId(pub [u8; 32]);
+
+impl TaskId {
+    pub fn base64encoded(&self) -> String {
+        let task_id_base64 = general_purpose::URL_SAFE_NO_PAD.encode(self.0);
+        task_id_base64
+    }
+}
+
+impl Decode for TaskId {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        // this should probably be available in codec...?
+        let mut data: [u8; 32] = [0; 32];
+        bytes.read_exact(&mut data)?;
+        Ok(TaskId(data))
+    }
+}
+
+impl Encode for TaskId {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        bytes.extend_from_slice(&self.0);
+    }
+}
+
+/// Time uint64;
+/// seconds elapsed since start of UNIX epoch
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-protocol-definition
+#[derive(Debug, PartialEq, Eq)]
+pub struct Time(pub u64);
+
+impl Decode for Time {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        Ok(Time(u64::decode(bytes)?))
+    }
+}
+
+impl Encode for Time {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        u64::encode(&self.0, bytes);
+    }
+}
+
+impl Time {
+    /// Generates a Time for the current system time rounded to the desired precision.
+    pub fn generate(time_precision: u64) -> Time {
+        let now_secs = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Failed to get time.")
+            .as_secs();
+        let timestamp = (now_secs / time_precision) * time_precision;
+        Time(timestamp)
+    }
+}
+
+/// struct {
+///     ExtensionType extension_type;
+///     opaque extension_data<0..2^16-1>;
+/// } Extension;
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-upload-extensions
+#[derive(Debug, PartialEq)]
+pub struct Extension {
+    extension_type: ExtensionType,
+    extension_data: Vec<u8>,
+}
+
+impl Decode for Extension {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        let extension_type = ExtensionType::from_u16(u16::decode(bytes)?);
+        let extension_data: Vec<u8> = decode_u16_items(&(), bytes)?;
+
+        Ok(Extension {
+            extension_type,
+            extension_data,
+        })
+    }
+}
+
+impl Encode for Extension {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        (self.extension_type as u16).encode(bytes);
+        encode_u16_items(bytes, &(), &self.extension_data);
+    }
+}
+
+/// enum {
+///     TBD(0),
+///     (65535)
+/// } ExtensionType;
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-upload-extensions
+#[derive(Debug, PartialEq, Clone, Copy)]
+#[repr(u16)]
+enum ExtensionType {
+    Tbd = 0,
+}
+
+impl ExtensionType {
+    fn from_u16(value: u16) -> ExtensionType {
+        match value {
+            0 => ExtensionType::Tbd,
+            _ => panic!("Unknown value for Extension Type: {}", value),
+        }
+    }
+}
+
+/// Identifier for a server's HPKE configuration
+/// uint8 HpkeConfigId;
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-protocol-definition
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub struct HpkeConfigId(u8);
+
+impl Decode for HpkeConfigId {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        Ok(HpkeConfigId(u8::decode(bytes)?))
+    }
+}
+
+impl Encode for HpkeConfigId {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        self.0.encode(bytes);
+    }
+}
+
+/// struct {
+///     HpkeConfigId id;
+///     HpkeKemId kem_id;
+///     HpkeKdfId kdf_id;
+///     HpkeAeadId aead_id;
+///     HpkePublicKey public_key;
+/// } HpkeConfig;
+/// opaque HpkePublicKey<1..2^16-1>;
+/// uint16 HpkeAeadId; /* Defined in [HPKE] */
+/// uint16 HpkeKemId;  /* Defined in [HPKE] */
+/// uint16 HpkeKdfId;  /* Defined in [HPKE] */
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-hpke-configuration-request
+#[derive(Debug)]
+pub struct HpkeConfig {
+    pub id: HpkeConfigId,
+    pub kem_id: u16,
+    pub kdf_id: u16,
+    pub aead_id: u16,
+    pub public_key: Vec<u8>,
+}
+
+impl Decode for HpkeConfig {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        Ok(HpkeConfig {
+            id: HpkeConfigId::decode(bytes)?,
+            kem_id: u16::decode(bytes)?,
+            kdf_id: u16::decode(bytes)?,
+            aead_id: u16::decode(bytes)?,
+            public_key: decode_u16_items(&(), bytes)?,
+        })
+    }
+}
+
+impl Encode for HpkeConfig {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        self.id.encode(bytes);
+        self.kem_id.encode(bytes);
+        self.kdf_id.encode(bytes);
+        self.aead_id.encode(bytes);
+        encode_u16_items(bytes, &(), &self.public_key);
+    }
+}
+
+/// An HPKE ciphertext.
+/// struct {
+///     HpkeConfigId config_id;    /* config ID */
+///     opaque enc<1..2^16-1>;     /* encapsulated HPKE key */
+///     opaque payload<1..2^32-1>; /* ciphertext */
+/// } HpkeCiphertext;
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-protocol-definition
+#[derive(Debug, PartialEq, Eq)]
+pub struct HpkeCiphertext {
+    pub config_id: HpkeConfigId,
+    pub enc: Vec<u8>,
+    pub payload: Vec<u8>,
+}
+
+impl Decode for HpkeCiphertext {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        let config_id = HpkeConfigId::decode(bytes)?;
+        let enc: Vec<u8> = decode_u16_items(&(), bytes)?;
+        let payload: Vec<u8> = decode_u32_items(&(), bytes)?;
+
+        Ok(HpkeCiphertext {
+            config_id,
+            enc,
+            payload,
+        })
+    }
+}
+
+impl Encode for HpkeCiphertext {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        self.config_id.encode(bytes);
+        encode_u16_items(bytes, &(), &self.enc);
+        encode_u32_items(bytes, &(), &self.payload);
+    }
+}
+
+/// uint8 ReportID[16];
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-protocol-definition
+#[derive(Debug, PartialEq, Eq)]
+pub struct ReportId(pub [u8; 16]);
+
+impl Decode for ReportId {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        let mut data: [u8; 16] = [0; 16];
+        bytes.read_exact(&mut data)?;
+        Ok(ReportId(data))
+    }
+}
+
+impl Encode for ReportId {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        bytes.extend_from_slice(&self.0);
+    }
+}
+
+impl ReportId {
+    pub fn generate() -> ReportId {
+        ReportId(rand::thread_rng().gen())
+    }
+}
+
+/// struct {
+///     ReportID report_id;
+///     Time time;
+///     Extension extensions<0..2^16-1>;
+/// } ReportMetadata;
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-upload-request
+#[derive(Debug, PartialEq)]
+pub struct ReportMetadata {
+    pub report_id: ReportId,
+    pub time: Time,
+    pub extensions: Vec<Extension>,
+}
+
+impl Decode for ReportMetadata {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        let report_id = ReportId::decode(bytes)?;
+        let time = Time::decode(bytes)?;
+        let extensions = decode_u16_items(&(), bytes)?;
+
+        Ok(ReportMetadata {
+            report_id,
+            time,
+            extensions,
+        })
+    }
+}
+
+impl Encode for ReportMetadata {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        self.report_id.encode(bytes);
+        self.time.encode(bytes);
+        encode_u16_items(bytes, &(), &self.extensions);
+    }
+}
+
+/// struct {
+///     TaskID task_id;
+///     ReportMetadata metadata;
+///     opaque public_share<0..2^32-1>;
+///     HpkeCiphertext encrypted_input_shares<1..2^32-1>;
+/// } Report;
+/// https://www.ietf.org/archive/id/draft-ietf-ppm-dap-02.html#name-upload-request
+#[derive(Debug, PartialEq)]
+pub struct Report {
+    pub task_id: TaskId,
+    pub metadata: ReportMetadata,
+    pub public_share: Vec<u8>,
+    pub encrypted_input_shares: Vec<HpkeCiphertext>,
+}
+
+// impl Report {
+//     /// Creates a minimal report for use in tests.
+//     pub fn new_dummy() -> Self {
+//         Report {
+//             task_id: TaskId([0x12; 32]),
+//             metadata: ReportMetadata {
+//                 report_id: ReportId::generate(),
+//                 time: Time::generate(1),
+//                 extensions: vec![],
+//             },
+//             public_share: vec![],
+//             encrypted_input_shares: vec![],
+//         }
+//     }
+// }
+
+impl Decode for Report {
+    fn decode(bytes: &mut Cursor<&[u8]>) -> Result<Self, CodecError> {
+        let task_id = TaskId::decode(bytes)?;
+        let metadata = ReportMetadata::decode(bytes)?;
+        let public_share: Vec<u8> = decode_u32_items(&(), bytes)?;
+        let encrypted_input_shares: Vec<HpkeCiphertext> = decode_u32_items(&(), bytes)?;
+
+        let remaining_bytes = bytes.get_ref().len() - (bytes.position() as usize);
+        if remaining_bytes == 0 {
+            Ok(Report {
+                task_id,
+                metadata,
+                public_share,
+                encrypted_input_shares,
+            })
+        } else {
+            Err(CodecError::BytesLeftOver(remaining_bytes))
+        }
+    }
+}
+
+impl Encode for Report {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        self.task_id.encode(bytes);
+        self.metadata.encode(bytes);
+        encode_u32_items(bytes, &(), &self.public_share);
+        encode_u32_items(bytes, &(), &self.encrypted_input_shares);
+    }
+}
